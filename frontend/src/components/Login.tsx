11e9e35d1a0f10f509177dc6c4b1174ad0905bb7
guibrbs <200037994@aluno.unb.br>
2022-07-03 17:24:46 -0300

    :rocket: Implementando os useStates

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,24 @@
 import { InputTemplate } from "./InputTemplate";
 import '../styles/authInput.css'
+import { useState } from "react";
 
 interface LoginInterface{
     setIsRegistering: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function Login( {setIsRegistering}: LoginInterface ){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return(
         <section className="auth-input-section">
             <h1 className="auth-input-title">Login</h1>
             <p className="auth-input-subtitle">Bem vindo! Por favor insira seus dados</p>
-            <InputTemplate type="Email" placeholder="Insira seu email" />
-            <InputTemplate type="Senha" placeholder="Insira sua senha" />
-            <button className="auth-button">Entrar</button>
+            <form className="form">
+                <InputTemplate type="Email" placeholder="Insira seu email" setProps={setEmail} />
+                <InputTemplate type="Senha" placeholder="Insira sua senha" setProps={setPassword} />
+                <button className="auth-button">Entrar</button>
+            </form>
             <p className="auth-connection">Não possui uma conta? 
                 <span className="contrast" onClick={() => setIsRegistering(true)}> Registre-se.</span>
             </p>
--- frontend/src/components/Logon.tsx
+++ frontend/src/components/Logon.tsx
@@ -1,22 +1,38 @@
 import { InputTemplate } from "./InputTemplate";
 import '../styles/authInput.css'
+import { useState } from "react";
+import { RegistrationPerformed } from "./RegistrationPerformed";
 
 interface LogonInterface{
     setIsRegistering: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function Logon( {setIsRegistering}: LogonInterface ){
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [successfulRegistration, setSuccessfulRegistration] = useState(false)
+
+    const handleSuccessfulRegistration = () => {
+        setSuccessfulRegistration(true)
+    }
+
     return(
         <section className="auth-input-section">
             <h1 className="auth-input-title">Registre-se</h1>
             <p className="auth-input-subtitle">Por favor insira seus dados para cadastro</p>
-            <InputTemplate type="Nome" placeholder="Insira seu nome completo" />
-            <InputTemplate type="Email" placeholder="Insira seu email" />
-            <InputTemplate type="Senha" placeholder="Insira sua senha" />
-            <button className="auth-button">Cadastrar</button>
+            <form className="form">
+                <InputTemplate type="Nome" placeholder="Insira seu nome completo" setProps={setName}/>
+                <InputTemplate type="Email" placeholder="Insira seu email" setProps={setEmail}/>
+                <InputTemplate type="Senha" placeholder="Insira sua senha" setProps={setPassword}/>
+                <button className="auth-button" onClick={handleSuccessfulRegistration}>Cadastrar</button>
+            </form>
             <p className="auth-connection">Já possui uma conta? 
                 <span className="contrast" onClick={() => setIsRegistering(false)}> Entre agora mesmo.</span>
             </p>
+            {
+                successfulRegistration ? <RegistrationPerformed /> : <></>
+            }
         </section>
     )
 }

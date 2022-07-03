c230d0581652a4d61ccd174bca8f3260cdc94ebb
guibrbs <200037994@aluno.unb.br>
2022-07-03 17:23:05 -0300

    :rocket: Adicionando props no template de input

--- frontend/src/components/InputTemplate/index.tsx
+++ frontend/src/components/InputTemplate/index.tsx
@@ -1,15 +1,26 @@
-import './styles.css'
+import "./styles.css";
 
-interface InputTemplateInterface{
-    type: string,
-    placeholder: string
+interface InputTemplateInterface {
+  type: string;
+  placeholder: string;
+  setProps: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export function InputTemplate( { type, placeholder }: InputTemplateInterface ){
-    return(
-        <>
-            <p>{type}</p>
-            <input required type={type === "Senha" ? "password" : "text"} placeholder={placeholder} className="auth-input" />
-        </>
-    )
+export function InputTemplate({
+  type,
+  placeholder,
+  setProps,
+}: InputTemplateInterface) {
+  return (
+    <>
+      <p>{type}</p>
+      <input
+        required
+        type={type === "Senha" ? "password" : "text"}
+        placeholder={placeholder}
+        className="auth-input"
+        onChange={(e) => setProps(e.target.value)}
+      />
+    </>
+  );
 }

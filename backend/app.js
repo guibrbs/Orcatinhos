282112681f875af94976f8e8ed4b671b0090cf53
Júlio César <juliodourado419@gmail.com]>
2022-07-09 17:43:56 -0300

    implemtentando cors e criando a rota usuarios

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
 
 const app = express()
 
 //Config JSON response
 app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(cors())
+
 
 // Models
 const User = require('./models/User')
@@ -15,6 +22,14 @@ const User = require('./models/User')
 //Public Route
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Bem vindo a nossa API!' })
+})
+
+
+app.get('/users', async (req, res)=> {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch(error){res.status(5000).send(error)}
 })
 
 // Private Route
@@ -145,7 +160,8 @@ app.post('/auth/login', async (req, res) => {
     },
       secret,
     )
-    res.status(200).json({ nameUser,
+    res.status(200).json({
+      nameUser,
       message: 'Autenticação realizada com sucesso!', token
     })
 
